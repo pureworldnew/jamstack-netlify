9325e4076a9937bf4da92cf9c111962821b5cfca
Pure World <pureworldnew@gmail.com>
2022-07-07 09:15:44 -0500

    first commit

---
+++ functions/todos-read.js
@@ -0,0 +1,31 @@
+/* Import faunaDB sdk */
+const faunadb = require("faunadb");
+const getId = require("./utils/getId");
+const q = faunadb.query;
+
+exports.handler = (event, context) => {
+  /* configure faunaDB Client with our secret */
+  const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SERVER_SECRET,
+    domain: "db.us.fauna.com",
+    scheme: "https",
+  });
+  const id = getId(event.path);
+  console.log(`Function 'todo-read' invoked. Read id: ${id}`);
+  return client
+    .query(q.Get(q.Ref(`classes/todos/${id}`)))
+    .then((response) => {
+      console.log("success", response);
+      return {
+        statusCode: 200,
+        body: JSON.stringify(response),
+      };
+    })
+    .catch((error) => {
+      console.log("error", error);
+      return {
+        statusCode: 400,
+        body: JSON.stringify(error),
+      };
+    });
+};
