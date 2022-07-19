a23a54acefdbee8f9e88fc29bdf992a97d521f47
Pure World <pureworldnew@gmail.com>
2022-07-19 05:30:31 -0500

    update React version into 18.2.0 latest one & convert class component into function component

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,17 +1,18 @@
-import Analytics from 'analytics'
-import googleAnalytics from '@analytics/google-analytics'
-const { REACT_APP_GOOGLE_ANALYTICS_ID } = process.env
-
-let plugins = []
+import Analytics from "analytics";
+import googleAnalytics from "@analytics/google-analytics";
+const { REACT_APP_GOOGLE_ANALYTICS_ID } = process.env;
+console.log("REACT_APP_GOOGLE_ANALYTICS_ID", REACT_APP_GOOGLE_ANALYTICS_ID);
+let plugins = [];
 // If google analytics ID set attach plugin
 if (REACT_APP_GOOGLE_ANALYTICS_ID) {
-  plugins = [ googleAnalytics({
-      trackingId: REACT_APP_GOOGLE_ANALYTICS_ID
-    })
-  ]
+  plugins = [
+    googleAnalytics({
+      trackingId: REACT_APP_GOOGLE_ANALYTICS_ID,
+    }),
+  ];
 }
 
 export default Analytics({
-  app: 'fauna-db-example',
-  plugins: plugins
-})
+  app: "fauna-db-example",
+  plugins: plugins,
+});
