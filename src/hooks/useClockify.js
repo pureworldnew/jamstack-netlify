5044c9f1e023a0653382e4ee14e68e69abafee9f
Pure World <pureworldnew@gmail.com>
2022-09-13 13:01:22 -0500

    encapsulate env clockify api key

--- src/hooks/useClockify.js
+++ src/hooks/useClockify.js
@@ -1,9 +1,11 @@
+const { REACT_APP_CLOCKIFY_API_KEY } = process.env;
+console.log("REACT_APP_CLOCKIFY_API_KEY", REACT_APP_CLOCKIFY_API_KEY);
 export default async function useClockify(url, method) {
   if (method !== "DELETE") {
     return await fetch(url, {
       method: method,
       headers: {
-        "X-API-KEY": "ZTJkN2JlM2UtNWQ3NS00ZTYyLWJiZWQtYjAyOGVjZDYxNGVl",
+        "X-API-KEY": REACT_APP_CLOCKIFY_API_KEY,
         Accept: "application/json",
         "Content-Type": "application/json",
       },
@@ -12,7 +14,7 @@ export default async function useClockify(url, method) {
     return await fetch(url, {
       method: method,
       headers: {
-        "X-API-KEY": "ZTJkN2JlM2UtNWQ3NS00ZTYyLWJiZWQtYjAyOGVjZDYxNGVl",
+        "X-API-KEY": REACT_APP_CLOCKIFY_API_KEY,
         Accept: "application/json",
         "Content-Type": "application/json",
       },
