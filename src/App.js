b5dcb8a5d90fb57ac1ab02b4355f9069f8698b35
Pure World <pureworldnew@gmail.com>
2022-09-14 03:57:45 -0500

    add error boundary feature using react-error-boundary

--- src/App.js
+++ src/App.js
@@ -7,29 +7,92 @@ import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
 import { Fallback } from "./components/fallback";
 import { AppBar, Drawer, Copyright } from "components/nav";
+import { withErrorBoundary } from "react-error-boundary";
+import Alert from "@mui/material/Alert";
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <Alert severity="error">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </Alert>
+  );
+}
 
 const PlanComponent = loadable(() => import("./feature/plan/Plan"), {
   fallback: <Fallback />,
+});
+
+const PlanComponentWithErrorBoundary = withErrorBoundary(PlanComponent, {
+  FallbackComponent: ErrorFallback,
+  onError(error, info) {
+    // Do something with the error
+    // E.g. log to an error logging client here
+  },
 });
 
 const CashComponent = loadable(() => import("./feature/cash/Cash"), {
   fallback: <Fallback />,
 });
 
+const CashComponentWithErrorBoundary = withErrorBoundary(CashComponent, {
+  FallbackComponent: ErrorFallback,
+  onError(error, info) {
+    // Do something with the error
+    // E.g. log to an error logging client here
+  },
+});
+
 const WorkComponent = loadable(() => import("./feature/work/Work"), {
   fallback: <Fallback />,
+});
+
+const WorkComponentWithErrorBoundary = withErrorBoundary(WorkComponent, {
+  FallbackComponent: ErrorFallback,
+  onError(error, info) {
+    // Do something with the error
+    // E.g. log to an error logging client here
+  },
 });
 
 const DashboardComponent = loadable(() => import("./feature/dashboard"), {
   fallback: <Fallback />,
 });
 
+const DashboardComponentWithErrorBoundary = withErrorBoundary(
+  DashboardComponent,
+  {
+    FallbackComponent: ErrorFallback,
+    onError(error, info) {
+      // Do something with the error
+      // E.g. log to an error logging client here
+    },
+  }
+);
+
 const StressComponent = loadable(() => import("./feature/stress/Stress"), {
   fallback: <Fallback />,
 });
 
+const StressComponentWithErrorBoundary = withErrorBoundary(StressComponent, {
+  FallbackComponent: ErrorFallback,
+  onError(error, info) {
+    // Do something with the error
+    // E.g. log to an error logging client here
+  },
+});
+
 const TrackComponent = loadable(() => import("./feature/track/Track"), {
   fallback: <Fallback />,
+});
+
+const TrackComponentWithErrorBoundary = withErrorBoundary(TrackComponent, {
+  FallbackComponent: ErrorFallback,
+  onError(error, info) {
+    // Do something with the error
+    // E.g. log to an error logging client here
+  },
 });
 
 export default function App() {
@@ -59,12 +122,30 @@ export default function App() {
           <Toolbar />
           <Container maxWidth="false" sx={{ mt: 4, mb: 4 }}>
             <Routes>
-              <Route path="/plan" element={<PlanComponent />} />
-              <Route path="/track" element={<TrackComponent />} />
-              <Route path="/stress" element={<StressComponent />} />
-              <Route path="/cash" element={<CashComponent />} />
-              <Route path="/work" element={<WorkComponent />} />
-              <Route path="/" element={<DashboardComponent />} />
+              <Route
+                path="/plan"
+                element={<PlanComponentWithErrorBoundary />}
+              />
+              <Route
+                path="/track"
+                element={<TrackComponentWithErrorBoundary />}
+              />
+              <Route
+                path="/stress"
+                element={<StressComponentWithErrorBoundary />}
+              />
+              <Route
+                path="/cash"
+                element={<CashComponentWithErrorBoundary />}
+              />
+              <Route
+                path="/work"
+                element={<WorkComponentWithErrorBoundary />}
+              />
+              <Route
+                path="/"
+                element={<DashboardComponentWithErrorBoundary />}
+              />
             </Routes>
             <Copyright sx={{ pt: 4 }} />
           </Container>
