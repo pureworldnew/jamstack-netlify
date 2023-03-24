fff4f1c33ed7d111d08f5856c5cd77ef46f9e475
crmonlinegems <pureworldnew@gmail.com>
2023-03-24 08:34:38 -0500

    user registration complete with validation

--- functions/auth-signup.js
+++ functions/auth-signup.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* Import faunaDB sdk */
 const faunadb = require("faunadb");
 
@@ -31,38 +32,54 @@ exports.handler = async (event, context) => {
 
       // check if user already exist
       // Validate if user exist in our database
-      // const oldUser = await User.findOne({ email });
-
-      // if (oldUser) {
-      //    return res.status(409).send("User Already Exist. Please Login");
-      // }
-      const salt = await bcrypt.genSalt();
-      const hashPassword = await bcrypt.hash(data.password, salt);
-      const userItem = {
-         data: { ...data, password: hashPassword },
-      };
-
-      console.log("hashPassword", hashPassword);
-      /* construct the fauna query */
       return client
-         .query(q.Create(q.Collection("user_entries"), userItem))
-         .then((response) => {
-            console.log("user_entries insertsuccess", response);
-            /* Success! return the response with statusCode 200 */
-            return {
-               statusCode: 200,
-               body: JSON.stringify(response),
+         .query(
+            q.Map(
+               q.Paginate(q.Match(q.Index("all_user_entries"), email)),
+               q.Lambda("user", q.Get(q.Var("user")))
+            )
+         )
+         .then(async (response) => {
+            console.log("response", response.data);
+            const oldUser = response.data;
+            if (oldUser.length) {
+               return {
+                  statusCode: 409,
+                  body: "User Already Exist. Please Login",
+               };
+            }
+            const salt = await bcrypt.genSalt();
+            const hashPassword = await bcrypt.hash(data.password, salt);
+            const userItem = {
+               data: { ...data, password: hashPassword },
             };
+
+            console.log("hashPassword", hashPassword);
+            /* construct the fauna query */
+            return client
+               .query(q.Create(q.Collection("user_entries"), userItem))
+               .then((res) => {
+                  console.log("user_entries insertsuccess", res);
+                  /* Success! return the res with statusCode 200 */
+                  return {
+                     statusCode: 201,
+                     body: JSON.stringify(res),
+                  };
+               })
+               .catch((error) => {
+                  console.log("error", error);
+                  /* Error! return the error with statusCode 400 */
+                  return {
+                     statusCode: 400,
+                     body: JSON.stringify(error),
+                  };
+               });
          })
-         .catch((error) => {
-            console.log("error", error);
-            /* Error! return the error with statusCode 400 */
-            return {
-               statusCode: 400,
-               body: JSON.stringify(error),
-            };
-         });
+         .catch((err) => console.log(err));
    } catch (err) {
-      console.log(err);
+      return {
+         statusCode: 400,
+         body: JSON.stringify(err),
+      };
    }
 };
