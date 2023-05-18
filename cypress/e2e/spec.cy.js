167008ceb43605d848156c19ff4dd202e09f8bac
crmonlinegems <pureworldnew@gmail.com>
2023-05-18 13:24:29 -0500

    adding e2e using cypress

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,23 @@
+describe("template spec", () => {
+   it("passes", () => {
+      cy.visit("http://localhost:8888/");
+      const signInBtn = cy.findByRole("button", { name: /Sign In/i });
+      const emailAddress = cy.findByRole("textbox", { name: /email/i });
+      cy.findByTestId("password");
+      cy.findByText("Sign in", { selector: "h1" });
+      cy.findByTestId("signInBackgroundImage");
+      const rememberMeCheckbox = cy.findByLabelText("Remember me");
+      cy.findByRole("link", {
+         name: "Forgot password?",
+      });
+      cy.findByRole("link", {
+         name: "Don't have an account? Sign Up",
+      });
+      cy.findByRole("link", {
+         name: "Your Website",
+      });
+      rememberMeCheckbox.should("not.be.checked");
+      emailAddress.should("be.visible");
+      signInBtn.click();
+   });
+});
