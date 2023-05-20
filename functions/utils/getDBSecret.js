11e9d2b6e62363f433d580e245ff34ed4810a38c
crmonlinegems <pureworldnew@gmail.com>
2023-05-19 21:59:32 -0500

    update environment for dev env

--- functions/utils/getDBSecret.js
+++ functions/utils/getDBSecret.js
@@ -1,11 +1,5 @@
 require("dotenv").config();
 
 module.exports = function getDBSecret() {
-   if (process.env.NODE_ENV === "development") {
-      return process.env.FAUNADB_SERVER_SECRET;
-   }
-   if (process.env.NODE_ENV === "production") {
-      return process.env.FAUNADB_SERVER_SECRET_ONLINE;
-   }
    return process.env.FAUNADB_SERVER_SECRET;
 };
