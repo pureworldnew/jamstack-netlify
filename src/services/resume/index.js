348dc31a1d8f43b1ff1f32f20c0c537fe0be3d10
Pure World <pureworldnew@gmail.com>
2023-11-24 19:50:03 -0600

    update again

--- src/services/resume/index.js
+++ src/services/resume/index.js
@@ -6,16 +6,6 @@ const create = (data) =>
       ...data,
    });
 
-const create1 = (data) =>
-   axios.post("/.netlify/functions/resume-create1", {
-      ...data,
-   });
-
-const create2 = (data) =>
-   axios.post("/.netlify/functions/resume-create2", {
-      ...data,
-   });
-
 const readAll = async () => {
    const response = await axios.get("/.netlify/functions/resume-read-all");
    return response.data;
@@ -23,7 +13,5 @@ const readAll = async () => {
 
 export default {
    create,
-   create1,
-   create2,
    readAll,
 };
